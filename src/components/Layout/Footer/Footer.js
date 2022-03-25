3ff26968decaa24dfa3bfae5afa4a17fc94833d0
Anton Vasylchenko <anton.vasylchenko.v@gmail.com>
2022-03-25 20:30:18 +0200

    some changes

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Card from '../../UI/Card';
+import TwitterIcon from '../../../assets/img/twitter.png';
+import GithubIcon from '../../../assets/img/github.png';
+import SteamIcon from '../../../assets/img/steam.png';
 
 import classes from './Footer.module.scss';
 
@@ -9,9 +12,31 @@ function Footer() {
     return (
         <Card>
             <div className={classes.footer}>
-                {year} | Created by Dikusor (:
+                <div className={classes.year}>
+                    {year}
+                </div>
+                <div className={classes.icons}>
+                    <a href="https://twitter.com/a_dikusor" target="_blank" rel="noreferrer">
+                        <div className={classes.icon}>
+                            <img src={TwitterIcon} alt="twitter" />
+                        </div>
+                    </a>
+
+                    <a href="https://github.com/Anton-Vasylchenko" target="_blank" rel="noreferrer">
+                        <div className={classes.icon}>
+                            <img src={GithubIcon} alt="github" />
+                        </div>
+                    </a>
+
+                    <a href="https://steamcommunity.com/id/ad_serpent/" target="_blank" rel="noreferrer">
+                        <div className={classes.icon}>
+                            <img src={SteamIcon} alt="steam" />
+                        </div>
+                    </a>
+
+                </div>
             </div>
-        </Card>
+        </Card >
     )
 }
 
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBIkmcit3xQDQkXJ_ZJwb5y3vqS1pNOWG4",
-    authDomain: "pixelart-demo.firebaseapp.com",
-    projectId: "pixelart-demo",
-    storageBucket: "pixelart-demo.appspot.com",
-    messagingSenderId: "395863447285",
-    appId: "1:395863447285:web:63c7136ca890c45f03d7bc",
-    measurementId: "G-NCYWC3VWPS"
+    apiKey: 'xxx',
+    authDomain: 'xxx',
+    projectId: 'xxx',
+    storageBucket: 'xxx',
+    messagingSenderId: 'xxx',
+    appId: 'xxx',
+    measurementId: 'xxx'
 };
 
 export default firebaseConfig;
